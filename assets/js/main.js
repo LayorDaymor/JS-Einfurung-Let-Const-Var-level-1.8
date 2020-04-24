bd6a69e8d9a2e34e097a0924d42ca1b6995a62fe
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 23:43:27 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let x=20;
+let y=30;
+let z=10;
+let resultOne=21
+x++
+y++
+console.log(x);
+console.log(y);
+console.log(y+x);
+y -=x
+console.log(y);
+x *=y
+console.log(x);
+x /=y
+console.log(x);
+x *=y ; 
+x /=z
+
+console.log(x)
